341f2ef63bac4ff74fffbbe956061d9ad7c2851e
Saran <romansaran446@gmail.com>
2021-01-12 09:46:58 +0530

    semantic ui cdn and array content rendered

---
+++ widgets/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+    {
+        title: 'What is React',
+        content: 'React is a front end javascript framework'
+    },
+    {
+        title: 'why use react',
+        content: 'React is a favourit JS libarary among engineers',
+
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use React by creating components'
+    },
+]
+
+        export default () => {
+
+            return (
+                <div>
+                <Accordion items={items} />
+                </div>
+            );
+        };
