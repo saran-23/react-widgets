63179f80362eb3250446b2a9903181123282e548
Saran <romansaran446@gmail.com>
2021-01-14 11:32:44 +0530

    navigation works without refresh the body

--- widgets/src/components/Route.js
+++ widgets/src/components/Route.js
@@ -1,12 +1,24 @@
-
+import { useEffect,useState } from "react";
 
 const Route =({path,children})=> {
+    const [currentPath,setCurrentPath] = useState(window.location.pathname);
+    useEffect(()=>{
 
-    return window.location.pathname === path 
+const onLocationChange = ()=> {
+    setCurrentPath(window.location.pathname);
+};
+
+            window.addEventListener('popstate', onLocationChange);
+            return () => {
+                window.removeEventListener('popstate',onLocationChange);
+            };
+    },[] );
+    return currentPath === path 
     ? children :  null;
         
     
 };
+
 
 export default Route;
 
