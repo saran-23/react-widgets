85889ce1fa9479ed98c59e4b07c656afa94b4569
Saran <romansaran446@gmail.com>
2021-01-14 08:21:26 +0530

    navigatonn link (react)

---
+++ widgets/src/components/Link.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Link =() => {
+    return(
+        <div>
+            <a>Link</a>
+        </div>
+    );
+}
+
+export default Link;
--- widgets/src/components/Route.js
+++ widgets/src/components/Route.js
@@ -1,4 +1,4 @@
-import Accordion from "./Accordion";
+
 
 const Route =({path,children})=> {
 
